3b43a0a633984ed0359d425a03637b175a40841d
tsemh <tiago_barbosa-silva@hotmail.com>
2023-10-31 21:44:50 -0300

    Iniciando

---
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/HrtechApiRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.nullkreativitat.hrtechapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrtechApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrtechApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nullkreativitat/hrtechapirest/HrtechApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nullkreativitat.hrtechapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrtechApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
