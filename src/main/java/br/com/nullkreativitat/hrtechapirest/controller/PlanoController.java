dcaa27d319742b1cffa8adb770b709fda6233020
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-05 19:08:27 -0300

    Criando classe controller plano

--- src/main/java/br/com/nullkreativitat/hrtechapirest/controller/PlanoController.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/controller/PlanoController.java
@@ -1,4 +1,36 @@
 package br.com.nullkreativitat.hrtechapirest.controller;
 
+import br.com.nullkreativitat.hrtechapirest.entity.Plano;
+import br.com.nullkreativitat.hrtechapirest.entity.Usuario;
+import br.com.nullkreativitat.hrtechapirest.repository.PlanoRepository;
+import br.com.nullkreativitat.hrtechapirest.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/plano")
 public class PlanoController {
+
+    @Autowired
+    private PlanoRepository planoRepository;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @PostMapping("/cadastrar")
+    public Plano postarPlano(@RequestBody Plano plano, @RequestParam long idUsuario) {
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(idUsuario);
+        if(usuarioOptional.isPresent()) {
+            Usuario usuario = usuarioOptional.get();
+            plano.setUsuario(usuario);
+            return planoRepository.save(plano);
+        } else {
+            throw new RuntimeException("Usuário não encontrado com o ID "+idUsuario);
+        }
+    }
 }
