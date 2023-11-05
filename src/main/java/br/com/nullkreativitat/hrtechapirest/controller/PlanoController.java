ec05072a99701cd6fb346bdc7f32ba28c68eace9
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-05 19:35:45 -0300

    Criando métodos get,update e delete

--- src/main/java/br/com/nullkreativitat/hrtechapirest/controller/PlanoController.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/controller/PlanoController.java
@@ -6,11 +6,10 @@ import br.com.nullkreativitat.hrtechapirest.repository.PlanoRepository;
 import br.com.nullkreativitat.hrtechapirest.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -33,4 +32,76 @@ public class PlanoController {
             throw new RuntimeException("Usuário não encontrado com o ID "+idUsuario);
         }
     }
+    @GetMapping("/obter-lista")
+    public List<Plano> obterTodosPlanos() {
+        return planoRepository.findAll();
+    }
+    @GetMapping("/obter-pelo-id/{id}")
+    public Plano obterPlanoPeloId(@PathVariable long id) {
+        return planoRepository.findById(id).get();
+    }
+    @GetMapping("/obter-pelo-nome")
+    public List<Plano> obterPeloNome(@RequestParam String nome) {
+        if(nome == null) {
+            throw new RuntimeException("Nome"+nome+"não foi encontrado");
+        }
+        return planoRepository.findByNome(nome);
+    }
+    @GetMapping("/obter-pelo-valor")
+    public List<Plano> obterPelovalor(@RequestParam Float valor) {
+        if(valor == null) {
+            throw new RuntimeException("Valor"+valor+"não foi encontrado");
+        }
+        return planoRepository.findByValor(valor);
+    }
+    @GetMapping("/obter-pela-validade")
+    public List<Plano> obterPelaValidade(@RequestParam Date validade) {
+        if(validade == null) {
+            throw new RuntimeException("Validade"+validade+"não foi encontrada");
+        }
+        return planoRepository.findByValidade(validade);
+    }
+    @GetMapping("/obter-pelo-usuario")
+    public List<Plano> obterPeloUsuario(@RequestParam Usuario usuario) {
+        if(usuario == null) {
+            throw new RuntimeException("Usuário"+usuario+"não foi encontrado");
+        }
+        return planoRepository.findByUsuario(usuario);
+    }
+    @PutMapping("/editar/{id}")
+    public Plano editarPlano(@RequestBody Plano plano, @PathVariable Long id) {
+        plano.setId(id);
+        return planoRepository.save(plano);
+    }
+    @DeleteMapping("deletar/{id}")
+    public void deletarPlano(@PathVariable Long id) {
+        planoRepository.deleteById(id);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
