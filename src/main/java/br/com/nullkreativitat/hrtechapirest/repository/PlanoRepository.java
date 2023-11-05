14c95cffd7a5890a54a6223a8b3679830d6d7d71
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-05 18:55:35 -0300

    Criando a classe repository plano

--- src/main/java/br/com/nullkreativitat/hrtechapirest/repository/PlanoRepository.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/repository/PlanoRepository.java
@@ -1,4 +1,16 @@
 package br.com.nullkreativitat.hrtechapirest.repository;
 
-public class PlanoRepository {
+import br.com.nullkreativitat.hrtechapirest.entity.Plano;
+import br.com.nullkreativitat.hrtechapirest.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Date;
+import java.util.List;
+
+public interface PlanoRepository extends JpaRepository<Plano, Long> {
+
+    List<Plano> findByNome(String nome);
+    List<Plano> fyndByValor(float valor);
+    List<Plano> findByValidade(Date validade);
+    List<Plano> findByUsuario(Usuario usuario);
 }
