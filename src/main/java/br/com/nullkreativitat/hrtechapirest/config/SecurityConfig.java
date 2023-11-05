e67a1a89765523732b6c3014608bbbf85572c0db
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-05 18:55:10 -0300

    Criando a classe entity plano

---
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/config/SecurityConfig.java
@@ -0,0 +1,2 @@
+package br.com.nullkreativitat.hrtechapirest.config;public class SecurityConfig {
+}
--- src/main/java/br/com/nullkreativitat/hrtechapirest/entity/Plano.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/entity/Plano.java
@@ -1,4 +1,30 @@
 package br.com.nullkreativitat.hrtechapirest.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name="tb_plano")
 public class Plano {
+
+    @Id
+    @Column(name="id_plano")
+    @SequenceGenerator(name="plano", sequenceName="sq_tb_plano", allocationSize=1)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="plano")
+    private long id;
+
+    @Column(name="ds_plano")
+    private String descricao;
+    @Column(name="nm_plano")
+    private String nome;
+    @Column(name="vl_plano")
+    private float valor;
+    @Column(name="dt_validade_plano")
+    private Date validade;
+    @ManyToOne
+    @JoinColumn(name="id_usuario")
+    @JsonBackReference(value="planos")
+    private Usuario usuario;
 }
