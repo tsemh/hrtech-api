540b50fde8356a3df02cfce67be65ba6380112aa
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-06 22:13:50 -0300

    Criando classe repository de cargo

--- src/main/java/br/com/nullkreativitat/hrtechapirest/repository/CargoRepository.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/repository/CargoRepository.java
@@ -1,2 +1,11 @@
-package br.com.nullkreativitat.hrtechapirest.repository;public class CargoRepository {
+package br.com.nullkreativitat.hrtechapirest.repository;
+
+import br.com.nullkreativitat.hrtechapirest.entity.Cargo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CargoRepository extends JpaRepository<Cargo, Long> {
+    List<Cargo> findByNome(String nome);
+    List<Cargo> findByNivel(Integer nivel);
 }
