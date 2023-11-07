9e1838e2ba973fae9e69133569031e7f10e76a50
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-06 22:14:13 -0300

    Criando classe repository de usuario

--- src/main/java/br/com/nullkreativitat/hrtechapirest/repository/UsuarioRepository.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/repository/UsuarioRepository.java
@@ -1,2 +1,13 @@
-package br.com.nullkreativitat.hrtechapirest.repository;public interface UsuarioRepository {
+package br.com.nullkreativitat.hrtechapirest.repository;
+
+import br.com.nullkreativitat.hrtechapirest.entity.Cargo;
+import br.com.nullkreativitat.hrtechapirest.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    List<Usuario> findByNome(String nome);
+    List<Usuario> findByEmail(String email);
+    List<Usuario> findByCargo(Cargo cargo);
 }
