c23f279bd934b772c0c2f56f7019dfcb9ec1bd12
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-06 22:13:02 -0300

    Criando classe entity cargo

--- src/main/java/br/com/nullkreativitat/hrtechapirest/entity/Cargo.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/entity/Cargo.java
@@ -1,2 +1,30 @@
-package br.com.nullkreativitat.hrtechapirest.entity;public class Cargo {
+package br.com.nullkreativitat.hrtechapirest.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="tb_cargo")
+public class Cargo {
+    @Id
+    @Column(name="id_cargo")
+    @SequenceGenerator(name="cargo", sequenceName="sq_tb_cargo", allocationSize=1)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="cargo")
+    private long id;
+    @Column(name="nm_cargo")
+    private String nome;
+    @Column(name="nv_cargo")
+    private Integer nivel;
+
+    public Cargo() {
+    }
+
+    public Cargo(long id, String nome, Integer nivel) {
+        this.id = id;
+        this.nome = nome;
+        this.nivel = nivel;
+    }
 }
