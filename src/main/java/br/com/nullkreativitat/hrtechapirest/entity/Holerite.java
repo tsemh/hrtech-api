2d97985664a0e1687547c5e4d39d2c03c6316e91
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-09 23:47:45 -0300

    Criando a classe entity holerite

--- src/main/java/br/com/nullkreativitat/hrtechapirest/entity/Holerite.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/entity/Holerite.java
@@ -1,2 +1,23 @@
-package br.com.nullkreativitat.hrtechapirest.entity;public class Holerite {
+package br.com.nullkreativitat.hrtechapirest.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name="tb_holerite")
+public class Holerite {
+    @Id
+    @Column(name="id_holerite")
+    @SequenceGenerator(name="holerite", sequenceName="sq_tb_holerite", allocationSize=1)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="holerite")
+    private Long id;
+    @Column(name="dt_holerite")
+    private Date date;
+    @Column(name="vl_holerite")
+    private Float valor;
 }
