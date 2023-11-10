b3dd6dc286a8c82dbe4a1e08009ca296be64bf1b
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-09 23:47:12 -0300

    Criando classe entity Reajuste

--- src/main/java/br/com/nullkreativitat/hrtechapirest/entity/Reajuste.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/entity/Reajuste.java
@@ -1,2 +1,26 @@
-package br.com.nullkreativitat.hrtechapirest.entity;public class Reajuste {
+package br.com.nullkreativitat.hrtechapirest.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="tb_reajuste")
+public class Reajuste {
+    @Id
+    @Column(name="id_reajuste")
+    @SequenceGenerator(name="reajuste", sequenceName="sq_tb_reajuste", allocationSize=1)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="reajuste")
+    private Long id;
+    @Column(name="nm_reajuste")
+    private String nome;
+    @Column(name="vl_reajuste")
+    private Float reajuste;
+    @ManyToOne
+    @JoinColumn(name="id_usuario")
+    @JsonBackReference(value="reajustes")
+    private Usuario usuario;
 }
