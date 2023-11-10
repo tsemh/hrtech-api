ed5cac6e3e8d1a8c9ed08e6227216b973bc83817
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-09 23:23:33 -0300

    Finalizando a classe noticia repository

--- src/main/java/br/com/nullkreativitat/hrtechapirest/repository/NoticiaRepository.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/repository/NoticiaRepository.java
@@ -1,2 +1,14 @@
-package br.com.nullkreativitat.hrtechapirest.repository;public class NoticiaRepository {
+package br.com.nullkreativitat.hrtechapirest.repository;
+
+import br.com.nullkreativitat.hrtechapirest.entity.Noticia;
+import br.com.nullkreativitat.hrtechapirest.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Date;
+import java.util.List;
+
+public interface NoticiaRepository extends JpaRepository<Noticia, Long> {
+    List<Noticia> findByNome(String nome);
+    List<Noticia> findByNome(Date data);
+    List<Noticia> findByUsuario(Usuario usuario);
 }
