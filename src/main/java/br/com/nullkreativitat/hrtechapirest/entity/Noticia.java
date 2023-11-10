9a76672734796c0e34090dfc123f1a1b119eb3ab
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-09 23:24:03 -0300

    Finalizando classe entity repository

--- src/main/java/br/com/nullkreativitat/hrtechapirest/entity/Noticia.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/entity/Noticia.java
@@ -2,9 +2,13 @@ package br.com.nullkreativitat.hrtechapirest.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
+@Getter
+@Setter
 @Entity
 @Table(name="tb_noticia")
 public class Noticia {
