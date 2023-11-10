d7caad234f23f971ab579c93417c12b00018e567
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-09 23:24:33 -0300

    Finalizando classe pontos de controller

--- src/main/java/br/com/nullkreativitat/hrtechapirest/controller/PontosController.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/controller/PontosController.java
@@ -1,2 +1,72 @@
-package br.com.nullkreativitat.hrtechapirest.controller;public class PontosController {
+package br.com.nullkreativitat.hrtechapirest.controller;
+
+import br.com.nullkreativitat.hrtechapirest.entity.Pontos;
+import br.com.nullkreativitat.hrtechapirest.repository.PontosRepository;
+import br.com.nullkreativitat.hrtechapirest.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/pontos")
+public class PontosController {
+
+    @Autowired
+    private PontosRepository pontosRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @PostMapping("/postar")
+    public Pontos post(@RequestBody Pontos ponto) {
+        ponto.setData(LocalDateTime.now());
+        return pontosRepository.save(ponto);
+    }
+    @GetMapping("/lista")
+    public List<Pontos> listarPontos() {
+        return pontosRepository.findAll();
+    }
+    @GetMapping("/pelo-id/{id}")
+    public ResponseEntity<Pontos> getById(@PathVariable Long id) {
+        Optional<Pontos> ponto = pontosRepository.findById(id);
+        return ponto.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+    @GetMapping("/pela-data")
+    public List<Pontos> obterPelaData(@RequestParam LocalDateTime data) {
+        return pontosRepository.findByData(data);
+    }
+    @GetMapping("/pelo-usuario")
+    public List<Pontos> obterPeloUsuario(@RequestParam Long idUsuario) {
+        return pontosRepository.findByUsuario(usuarioRepository.findById(idUsuario).orElse(null));
+    }
+    @PutMapping("editar/{id}")
+    public ResponseEntity<Pontos> editarPlano(@PathVariable Long id, @RequestBody Pontos novoPonto) {
+        Optional<Pontos> ponto = pontosRepository.findById(id);
+
+        if (ponto.isPresent()) {
+            Pontos pontoExistente = ponto.get();
+            pontoExistente.setPontp(novoPonto.getPontp());
+            pontoExistente.setData(novoPonto.getData());
+            pontoExistente.setUsuario(novoPonto.getUsuario());
+
+            return new ResponseEntity<>(pontosRepository.save(pontoExistente), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @DeleteMapping("deletar/{id}")
+    public ResponseEntity<Void> deletarPlano(@PathVariable Long id) {
+        if (pontosRepository.existsById(id)) {
+            pontosRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
