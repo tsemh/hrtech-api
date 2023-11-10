111c849e748256cc91c504ab623acab3f4181073
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-09 23:23:10 -0300

    Finalizando a classe pontos repository

--- src/main/java/br/com/nullkreativitat/hrtechapirest/repository/PontosRepository.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/repository/PontosRepository.java
@@ -1,2 +1,13 @@
-package br.com.nullkreativitat.hrtechapirest.repository;public class PontosRepository {
+package br.com.nullkreativitat.hrtechapirest.repository;
+
+import br.com.nullkreativitat.hrtechapirest.entity.Pontos;
+import br.com.nullkreativitat.hrtechapirest.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface PontosRepository extends JpaRepository<Pontos, Long> {
+    List<Pontos> findByData(LocalDateTime data);
+    List<Pontos> findByUsuario(Usuario usuario);
 }
