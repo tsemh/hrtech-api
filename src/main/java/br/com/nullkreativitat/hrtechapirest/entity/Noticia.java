61f18997f33eb19319d72ae1e4f9b3b9cbb11a16
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-09 23:07:40 -0300

    Criando a classe entity noticia

---
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/entity/Noticia.java
@@ -0,0 +1,26 @@
+package br.com.nullkreativitat.hrtechapirest.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name="tb_noticia")
+public class Noticia {
+    @Id
+    @Column(name="id_noticia")
+    @SequenceGenerator(name="noticia", sequenceName="sq_tb_noticia", allocationSize=1)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="noticia")
+    private Long id;
+    @Column(name="nm_noticia")
+    private String nome;
+    @Column(name="ds_noticia")
+    private String descricao;
+    @Column(name="dt_noticia")
+    private Date date;
+    @ManyToOne
+    @JoinColumn(name="id_usuario")
+    @JsonBackReference(value="noticias")
+    private Usuario usuario;
+}
---
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/repository/PontosRepository.java
@@ -0,0 +1,2 @@
+package br.com.nullkreativitat.hrtechapirest.repository;public class PontosRepository {
+}
