18359e34666f5aa4f2536369ca79adb74fa87eca
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-11 10:36:09 -0300

    Criando a classe repository de criterio

--- src/main/java/br/com/nullkreativitat/hrtechapirest/repository/CriterioRepository.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/repository/CriterioRepository.java
@@ -1,2 +1,13 @@
-package br.com.nullkreativitat.hrtechapirest.repository;public class CriterioRepository {
+package br.com.nullkreativitat.hrtechapirest.repository;
+
+import br.com.nullkreativitat.hrtechapirest.entity.Criterio;
+import br.com.nullkreativitat.hrtechapirest.entity.ProcessoSeletivo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CriterioRepository extends JpaRepository<Criterio, Long> {
+    List<Criterio> findByNome(String nome);
+    List<Criterio> findByValor(Integer valor);
+    List<Criterio> findByProcessoSeletivo(ProcessoSeletivo processoSeletivo);
 }
