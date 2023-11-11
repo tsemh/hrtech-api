e1aa7eb4508ef6afed886a087874dabad02ae952
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-11 10:36:41 -0300

    Criando a classe repository de processoSeletivo

--- src/main/java/br/com/nullkreativitat/hrtechapirest/repository/ProcessoSeletivoRepository.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/repository/ProcessoSeletivoRepository.java
@@ -1,2 +1,19 @@
-package br.com.nullkreativitat.hrtechapirest.repository;public class ProcessoSeletivoRepository {
+package br.com.nullkreativitat.hrtechapirest.repository;
+
+import br.com.nullkreativitat.hrtechapirest.entity.Cargo;
+import br.com.nullkreativitat.hrtechapirest.entity.ProcessoSeletivo;
+import br.com.nullkreativitat.hrtechapirest.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.beans.PropertyEditor;
+import java.util.Date;
+import java.util.List;
+
+public interface ProcessoSeletivoRepository extends JpaRepository<ProcessoSeletivo, Long> {
+    List<ProcessoSeletivo> findByNome(String nome);
+    List<ProcessoSeletivo> findByDataInicio(Date dataInicio);
+    List<ProcessoSeletivo> findByDataFim(Date dataFim);
+    List<PropertyEditor> findByNumeroVagas(Integer numeroVagas);
+    List<ProcessoSeletivo> findByUsuario(Usuario usuario);
+    List<ProcessoSeletivo> findByCargo(Cargo cargo);
 }
