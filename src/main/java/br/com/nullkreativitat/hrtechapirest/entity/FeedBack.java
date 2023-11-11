7d9de0ca8bb45841d144f05cc44b5419160c329c
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-11 08:10:44 -0300

    Criando a classe entity FeedBack

---
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/entity/FeedBack.java
@@ -0,0 +1,44 @@
+package br.com.nullkreativitat.hrtechapirest.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name="tb_feedback")
+public class FeedBack {
+    @Id
+    @Column(name="id_feedback")
+    @SequenceGenerator(name="feedback", sequenceName="sq_tb_feedback", allocationSize=1)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="feedback")
+    private Long id;
+    @Column(name="ds_feedback")
+    private String descricao;
+    @Column(name="nm_feedback")
+    private String nm_feedback;
+    @Column(name="dt_feedback")
+    private Date data;
+    @Column(name="st_feedback")
+    private String status;
+    @OneToOne
+    @JoinColumn(name="id_usuario")
+    @JsonBackReference(value="feedback")
+    private Usuario usuario;
+
+    public FeedBack() {
+    }
+
+    public FeedBack(Long id, String descricao, String nm_feedback, Date data, String status, Usuario usuario) {
+        this.id = id;
+        this.descricao = descricao;
+        this.nm_feedback = nm_feedback;
+        this.data = data;
+        this.status = status;
+        this.usuario = usuario;
+    }
+}
