5cb0a049e30dc316df720d871297b9cbf8485345
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-10 21:56:03 -0300

    Criando a clase repository de vale

--- src/main/java/br/com/nullkreativitat/hrtechapirest/repository/ValeRepository.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/repository/ValeRepository.java
@@ -1,2 +1,15 @@
-package br.com.nullkreativitat.hrtechapirest.repository;public class ValeRepository {
+package br.com.nullkreativitat.hrtechapirest.repository;
+
+import br.com.nullkreativitat.hrtechapirest.entity.Usuario;
+import br.com.nullkreativitat.hrtechapirest.entity.Vale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Date;
+import java.util.List;
+
+public interface ValeRepository extends JpaRepository<Vale, Long> {
+    List<Vale> findByNome(String nome);
+    List<Vale> findByValor(Float valor);
+    List<Vale> findByData(Date data);
+    List<Vale> findByUsuario(Usuario usuario);
 }
