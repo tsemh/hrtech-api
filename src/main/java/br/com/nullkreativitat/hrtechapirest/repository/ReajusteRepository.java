6e72512897cd4b833811c5069262d18a903012cb
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-10 21:56:34 -0300

    Criando a classe llkreativitat/hrtechapirest/repository/ReajusteRepository.java

--- src/main/java/br/com/nullkreativitat/hrtechapirest/repository/ReajusteRepository.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/repository/ReajusteRepository.java
@@ -1,2 +1,13 @@
-package br.com.nullkreativitat.hrtechapirest.repository;public class ReajusteRepository {
+package br.com.nullkreativitat.hrtechapirest.repository;
+
+import br.com.nullkreativitat.hrtechapirest.entity.Holerite;
+import br.com.nullkreativitat.hrtechapirest.entity.Reajuste;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReajusteRepository extends JpaRepository<Reajuste, Long> {
+    List<Reajuste> findByNome(String nome);
+    List<Reajuste> findByValor(Float valor);
+    List<Reajuste> findByHolerite(Holerite holerite);
 }
