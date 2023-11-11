a7869978e5bf513bf8dc8121560872082ea6d4d9
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-11 10:35:27 -0300

    Criando a classe entity de critério

--- src/main/java/br/com/nullkreativitat/hrtechapirest/entity/Criterio.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/entity/Criterio.java
@@ -1,2 +1,36 @@
-package br.com.nullkreativitat.hrtechapirest.entity;public class Criterio {
+package br.com.nullkreativitat.hrtechapirest.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="tb_criterio")
+public class Criterio {
+    @Id
+    @Column(name="id_criterio")
+    @SequenceGenerator(name="criterio", sequenceName="sq_tb_criterio", allocationSize=1)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="criterio")
+    private Long id;
+    @Column(name="nm_criterio")
+    private String nome;
+    @Column(name="vl_criterio")
+    private Integer valor;
+    @ManyToOne
+    @JoinColumn(name="id_processo")
+    @JsonBackReference(value="criterios")
+    private ProcessoSeletivo processoSeletivo;
+
+    public Criterio() {
+    }
+
+    public Criterio(Long id, String nome, Integer valor, ProcessoSeletivo processoSeletivo) {
+        this.id = id;
+        this.nome = nome;
+        this.valor = valor;
+        this.processoSeletivo = processoSeletivo;
+    }
 }
