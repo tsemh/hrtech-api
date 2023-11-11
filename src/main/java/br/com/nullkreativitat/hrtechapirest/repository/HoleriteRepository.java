97cc677123e3d25faf8ca2578157eb287461300f
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-10 21:57:30 -0300

    Criando a classe repository de holerite

--- src/main/java/br/com/nullkreativitat/hrtechapirest/repository/HoleriteRepository.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/repository/HoleriteRepository.java
@@ -1,2 +1,15 @@
-package br.com.nullkreativitat.hrtechapirest.repository;public enum HoleriteRepository {
+package br.com.nullkreativitat.hrtechapirest.repository;
+
+import br.com.nullkreativitat.hrtechapirest.entity.Cargo;
+import br.com.nullkreativitat.hrtechapirest.entity.Holerite;
+import br.com.nullkreativitat.hrtechapirest.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Date;
+import java.util.List;
+
+public interface HoleriteRepository extends JpaRepository<Holerite, Long> {
+    List<Holerite> findByData(Date data);
+    List<Holerite> findByValor(Float valor);
+    List<Holerite> findByUsuario(Usuario usuario);
 }
