337c5ab05db4997a4240362c41973e68df50b4f8
tsemh <tiago_barbosa-silva@hotmail.com>
2023-11-21 21:57:22 -0300

    teste

--- src/main/java/br/com/nullkreativitat/hrtechapirest/repository/PontosRepository.java
+++ src/main/java/br/com/nullkreativitat/hrtechapirest/repository/PontosRepository.java
@@ -11,6 +11,12 @@ import java.util.List;
 public interface PontosRepository extends JpaRepository<Pontos, Long> {
     List<Pontos> findByData(LocalDateTime data);
     List<Pontos> findByUsuario(Usuario usuario);
-    @Query("SELECT p FROM Pontos p WHERE p.data = :data AND p.usuario = :usuario")
-    Pontos findByDataAndUsuario(@Param("data") LocalDateTime data, @Param("usuario") Usuario usuario);
+    @Query("SELECT p FROM Pontos p WHERE YEAR(p.data) = YEAR(:data) " +
+           "AND MONTH(p.data) = MONTH(:data) " +
+           "AND DAY(p.data) = DAY(:data) " +
+           "AND HOUR(p.data) = HOUR(:data) " +
+           "AND MINUTE(p.data) = MINUTE(:data) " +
+           "AND SECOND(p.data) = SECOND(:data) " +
+           "AND p.usuario = :usuario")
+    List<Pontos> findByDataAndUsuario(@Param("data") LocalDateTime data, @Param("usuario") Usuario usuario);
 }
